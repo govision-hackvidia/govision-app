aa48add0feacadb3e228de7e44ac9cd836042ad0
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-05-02 15:37:47 +0700

    System style

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:govision/app/app.dart';
 import 'package:govision/bootstrap.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   bootstrap(() => const App());
 }
