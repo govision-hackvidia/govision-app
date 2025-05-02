cdb672c565d68c225f88285bfa3673241a83e766
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-05-02 14:24:03 +0700

    Initialize project

---
+++ android/app/src/main/kotlin/id/web/govision/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.web.govision
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:govision/app/app.dart';
+import 'package:govision/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
